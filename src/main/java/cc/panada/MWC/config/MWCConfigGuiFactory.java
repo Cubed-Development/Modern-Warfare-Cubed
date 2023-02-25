1a0f9f3acdd2cc825e452e3509fae660ba92ef68
Desoroxxx <desoroxxx@gmail.com>
2023-02-25 20:24:31 +0100

    Initial Commit

---
+++ src/main/java/cc/panada/MWC/config/MWCConfigGuiFactory.java
@@ -0,0 +1,30 @@
+package cc.panada.MWC.config;
+
+//import cc.panada.MWC.gui.menu.Page0;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.fml.client.IModGuiFactory;
+
+import java.util.Set;
+
+public class MWCConfigGuiFactory implements IModGuiFactory {
+
+    @Override
+    public void initialize(Minecraft minecraftInstance) {
+    }
+
+    @Override
+    public boolean hasConfigGui() {
+        return true;
+    }
+
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen) {
+        return /*new Page0()*/ null;
+    }
+
+    @Override
+    public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
+        return null;
+    }
+}
