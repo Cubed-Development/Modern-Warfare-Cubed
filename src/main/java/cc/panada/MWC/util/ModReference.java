253adb81b3e4f2c9cd2527b957be44182e93e328
SticksPluto <Stickandbiscuit@gmail.com>
2023-02-26 15:00:44 -0800

    Description Summary
    #Added
    DuplicateStrategy for the mcmod.info
    An mcmod.info File
    A pack.mcmeta (for en_us.lang instead of en_US.lang) File
    
    #Fixed
    Mod ID Being all CAPS
    Changed assets.mwc to
    assets, mwc for separate folders instead of a glitch same with textures.gui

--- src/main/java/cc/panada/MWC/util/ModReference.java
+++ src/main/java/cc/panada/MWC/util/ModReference.java
@@ -9,7 +9,7 @@ import java.net.URISyntaxException;
 public class ModReference {
 
 
-    public static final String MOD_ID = "MWC";
+    public static final String MOD_ID = "mwc";
     public static final String NAME = "Modern Warfare Cubed";
     public static final String VERSION = "@VERSION@";
     public static final Logger MWC_LOG = LogManager.getLogger(MOD_ID);
