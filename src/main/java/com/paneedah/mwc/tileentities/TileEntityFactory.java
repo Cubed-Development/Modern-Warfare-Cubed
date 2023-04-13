c29eab72060b58b414abfc6129315d69e99607cb
SticksDev <Stickandbiscuit@gmail.com>
2023-04-13 14:15:47 -0700

    Removed - Props | Turrets, Fixed AnimationGUI warnings

--- src/main/java/com/paneedah/mwc/tileentities/TileEntityFactory.java
+++
@@ -1,9 +0,0 @@
-package com.paneedah.mwc.tileentities;
-
-import com.paneedah.weaponlib.ModContext;
-
-public interface TileEntityFactory {
-
-    void createTileEntity(ModContext modContext);
-    
-}
