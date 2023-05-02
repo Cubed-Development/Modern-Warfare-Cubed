011ddb70af41849cae5ff5bdd41d45269509c865
SticksDev <Stickandbiscuit@gmail.com>
2023-05-01 23:49:06 -0700

    Removed all props, disabled turrets due to a dupe
    Took 40 minutes

--- src/main/java/com/paneedah/mwc/tileentities/TurretBaseFactory.java
+++ src/main/java/com/paneedah/mwc/tileentities/TurretBaseFactory.java
@@ -12,24 +12,24 @@ public class TurretBaseFactory implements TileEntityFactory {
 
     @Override
     public void createTileEntity(ModContext modContext) {
-    	new LootBoxConfiguration()
-        .withMaterial(CompatibleMaterial.ROCK)
-        .withName("turret_base")
-        .withModelClassName("com.paneedah.mwc.models.TurretBase")
-        .withTextureName("textures/models/turretbase.png")
-//        .withEquipementDispenseSound(sound)
-        .withCreativeTab(ModernWarfareMod.PropsTab)
-        .withBoundingBox(0.0, 0, 0.0, 1, 0.2, 1)
-        .withPositioning(tileEntity -> {
-//            GL11.glScalef(0.5f, 0.5f, 0.5f);
-            GL11.glTranslatef(0.5f, 0f, 0.5f);
-//            GL11.glRotatef(-45F, 0f, 1f, 0f);
-        })
-//        .withEquipmentOption(Guns.M9A1, EnumDifficulty.EASY, 4f)
-//        .withEquipmentOption(Guns.M45A1, EnumDifficulty.EASY, 3f)
-//        .withEquipmentOption(Items.air, EnumDifficulty.EASY, 150f, 1)
-//        .withEquipmentDispenseTimeout(10)
-        .build(ModernWarfareMod.MOD_CONTEXT);
+//    	new LootBoxConfiguration()
+//        .withMaterial(CompatibleMaterial.ROCK)
+//        .withName("turret_base")
+//        .withModelClassName("com.paneedah.mwc.models.TurretBase")
+//        .withTextureName("textures/models/turretbase.png")
+////        .withEquipementDispenseSound(sound)
+//        .withCreativeTab(ModernWarfareMod.PropsTab)
+//        .withBoundingBox(0.0, 0, 0.0, 1, 0.2, 1)
+//        .withPositioning(tileEntity -> {
+////            GL11.glScalef(0.5f, 0.5f, 0.5f);
+//            GL11.glTranslatef(0.5f, 0f, 0.5f);
+////            GL11.glRotatef(-45F, 0f, 1f, 0f);
+//        })
+////        .withEquipmentOption(Guns.M9A1, EnumDifficulty.EASY, 4f)
+////        .withEquipmentOption(Guns.M45A1, EnumDifficulty.EASY, 3f)
+////        .withEquipmentOption(Items.air, EnumDifficulty.EASY, 150f, 1)
+////        .withEquipmentDispenseTimeout(10)
+//        .build(ModernWarfareMod.MOD_CONTEXT);
     }
     
     @SubscribeEvent
