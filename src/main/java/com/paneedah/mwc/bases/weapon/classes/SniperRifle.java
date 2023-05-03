b0dffd4395c451eda7d911bcfa68eed7bed7eaea
Azurae <108265293+azuraerae@users.noreply.github.com>
2023-05-03 16:22:45 +0200

    Freeing the weapons from the CompatLayer! Setting up the base of the weapon system I'm still brainstorming ways to make it work though, but don't hesitate to send me ways in discord
    - Raeilgun (Azurae)

---
+++ src/main/java/com/paneedah/mwc/bases/weapon/classes/SniperRifle.java
@@ -0,0 +1,9 @@
+package com.paneedah.mwc.bases.weapon.classes;
+
+import com.paneedah.mwc.bases.weapon.WeaponBase;
+
+public class SniperRifle extends WeaponBase {
+    public SniperRifle(String registryName) {
+        super(registryName);
+    }
+}
