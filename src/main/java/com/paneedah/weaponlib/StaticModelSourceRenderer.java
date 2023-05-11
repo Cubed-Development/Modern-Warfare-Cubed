35598b2f5b1fd675dba196431510e1c5988149c0
Desoroxxx <desoroxxx@gmail.com>
2023-05-11 04:27:33 +0200

    Revert "Remove `CompatibleStaticModelSourceRenderer`"
    This reverts commit 69176d6b6a3c14dfef64a77858ab20a287d10738.

--- src/main/java/com/paneedah/weaponlib/StaticModelSourceRenderer.java
+++ src/main/java/com/paneedah/weaponlib/StaticModelSourceRenderer.java
@@ -1,70 +1,21 @@
 package com.paneedah.weaponlib;
 
-import com.paneedah.mwc.utils.ModReference;
 import com.paneedah.weaponlib.animation.MultipartPositioning.Positioner;
-import com.paneedah.weaponlib.compatibility.CompatibleWeaponRenderer;
+import com.paneedah.weaponlib.compatibility.CompatibleStaticModelSourceRenderer;
 import com.paneedah.weaponlib.compatibility.Interceptors;
-import com.paneedah.weaponlib.compatibility.ModelSourceRenderer;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.model.ModelBase;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.block.model.*;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-import org.apache.commons.lang3.tuple.Pair;
 import org.lwjgl.opengl.GL11;
 
-import javax.vecmath.Matrix4f;
-import java.util.Collections;
-import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-import static com.paneedah.mwc.proxies.ClientProxy.mc;
 import static com.paneedah.weaponlib.compatibility.CompatibilityProvider.compatibility;
 
-public class StaticModelSourceRenderer extends ModelSourceRenderer implements IBakedModel {
-
-	private class WeaponItemOverrideList extends ItemOverrideList {
-
-		public WeaponItemOverrideList(List<ItemOverride> overridesIn) {
-			super(overridesIn);
-		}
-
-		@Override
-		public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack, World world,
-										   EntityLivingBase entity) {
-			StaticModelSourceRenderer.this.itemStack = stack;
-			StaticModelSourceRenderer.this.owner = entity;
-			return super.handleItemState(originalModel, stack, world, entity);
-		}
-	}
-
-	protected Builder builder;
-
-	private Pair<? extends IBakedModel, Matrix4f> pair;
-
-	protected EntityLivingBase owner;
-
-	protected ItemStack itemStack;
-
-	private ItemOverrideList itemOverrideList = new WeaponItemOverrideList(Collections.emptyList());
-
-	protected ItemCameraTransforms.TransformType transformType;
+public class StaticModelSourceRenderer extends CompatibleStaticModelSourceRenderer {
 
 	public static class Builder {
-
 		private Consumer<ItemStack> entityPositioning;
 		private Consumer<ItemStack> inventoryPositioning;
 		private BiConsumer<EntityPlayer, ItemStack> thirdPersonPositioning;
@@ -79,6 +30,7 @@ public class StaticModelSourceRenderer extends ModelSourceRenderer implements IB
 		
 		private Consumer<RenderContext<RenderableState>> firstPersonLeftHandPositioning;
 		private Consumer<RenderContext<RenderableState>> firstPersonRightHandPositioning;
+		
 
 		private ModContext modContext;
 		private boolean isHiddenInInventory;
@@ -89,7 +41,7 @@ public class StaticModelSourceRenderer extends ModelSourceRenderer implements IB
 		}
 		
 		public Builder withModContext(ModContext modContext) {
-			this.modContext = modContext;
+		    this.modContext = modContext;
 		    return this;
 		}
 
@@ -248,14 +200,14 @@ public class StaticModelSourceRenderer extends ModelSourceRenderer implements IB
 	}
 
 	private StaticModelSourceRenderer(Builder builder) {
-		this.builder = builder;
-		this.pair = Pair.of((IBakedModel) this, null);
+		super(builder);
 	}
 
-	protected ModContext getModContext() {
-		return builder.modContext;
-	}
-
+    @Override
+    protected ModContext getModContext() {
+        return builder.modContext;
+    }
+    
     public void renderCustomEquipped(EntityPlayer player, ItemStack itemStack) {
         
     	
@@ -298,203 +250,4 @@ public class StaticModelSourceRenderer extends ModelSourceRenderer implements IB
         
         GL11.glPopMatrix();
     }
-
-	@Override
-	public List<BakedQuad> getQuads(IBlockState state, EnumFacing side, long rand) {
-		// Todo: Actually make rendering compatible with Emissive Renderer
-		if (net.minecraftforge.common.ForgeModContainer.allowEmissiveItems) {
-			return Collections.emptyList();
-		}
-
-		if(itemStack == null) return Collections.emptyList();
-		if(transformType == null
-				|| transformType == ItemCameraTransforms.TransformType.GROUND
-				|| transformType == ItemCameraTransforms.TransformType.GUI
-				|| transformType == ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND
-				|| transformType == ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND
-		) {
-
-			Tessellator tessellator = Tessellator.getInstance();
-			BufferBuilder worldrenderer = tessellator.getBuffer();
-			tessellator.draw();
-			GlStateManager.pushMatrix();
-
-			if (owner != null) {
-				if (transformType == ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND) {
-
-					if (owner.isSneaking()) GlStateManager.translate(0.0F, -0.2F, 0.0F);
-				}
-			}
-
-			if (onGround()) {
-				GlStateManager.scale(-3f, -3f, -3f);
-			}
-
-			int currentTextureId = GlStateManager.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
-			if(transformType == null && owner instanceof EntityPlayer) {
-				renderCustomEquipped((EntityPlayer)owner, itemStack);
-			} else {
-				renderItem();
-			}
-			if(currentTextureId != 0) {
-				GlStateManager.bindTexture(currentTextureId);
-			}
-			GlStateManager.popMatrix();
-			worldrenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.ITEM);
-		}
-
-		// Reset the dynamic values.
-		this.owner = null;
-		this.itemStack = null;
-		this.transformType = null;
-
-		return Collections.emptyList();
-	}
-
-	@Override
-	public boolean isAmbientOcclusion() {
-		return true;
-	}
-
-	@Override
-	public boolean isGui3d() {
-		return true;
-	}
-
-	@Override
-	public boolean isBuiltInRenderer() {
-		return false;
-	}
-
-	@Override
-	public TextureAtlasSprite getParticleTexture() {
-		return mc.getTextureMapBlocks().getMissingSprite();
-	}
-
-	@Override
-	public ItemOverrideList getOverrides() {
-		return itemOverrideList;
-	}
-
-	protected boolean onGround() {
-		return transformType == null;
-	}
-
-	@SideOnly(Side.CLIENT)
-	public void renderItem()
-	{
-
-		GL11.glPushMatrix();
-
-		GL11.glScaled(-1F, -1F, 1F);
-
-		EntityPlayer player = compatibility.clientPlayer();
-		RenderContext<RenderableState> renderContext = new RenderContext<>(getModContext(), player, itemStack);
-
-		switch (transformType)
-		{
-			case GROUND:
-				GL11.glScaled(0.35F, 0.35F, 0.35F);
-				GL11.glTranslatef(-0.7f, -1f, -0.1f);
-				GL11.glRotatef(0F, 1f, 0f, 0f);
-				//GL11.glRotatef(150F, 0f, 1f, 0f);
-				GL11.glRotatef(90F, 0f, 0f, 1f);
-				builder.getEntityPositioning().accept(itemStack);
-				break;
-			case GUI:
-				GL11.glScaled(0.6F, 0.6F, 0.6F);
-				GL11.glTranslatef(-0.7f, -0.8f, -0.1f);
-				GL11.glRotatef(-30F, 1f, 0f, 0f);
-				GL11.glRotatef(40F, 0f, 1f, 0f);
-				GL11.glRotatef(0F, 0f, 0f, 1f);
-				builder.getInventoryPositioning().accept(itemStack);
-				break;
-			case THIRD_PERSON_RIGHT_HAND: case THIRD_PERSON_LEFT_HAND:
-			GL11.glScaled(0.4F, 0.4F, 0.4F);
-			GL11.glTranslatef(-1.5f, -2.4f, 1.3f);
-			GL11.glRotatef(-100F, 1f, 0f, 0f);
-			GL11.glRotatef(50F, 0f, 1f, 0f);
-			GL11.glRotatef(0F, 0f, 0f, 1f);
-			builder.getThirdPersonPositioning().accept(player, itemStack);
-			break;
-			case FIRST_PERSON_RIGHT_HAND: case FIRST_PERSON_LEFT_HAND:
-			GL11.glScaled(0.6F, 0.6F, 0.6F);
-			GL11.glRotatef(-45F, 0f, 1f, 0f);
-			GL11.glTranslatef(-0.3f, -0.855f, 0.5f);
-			builder.getFirstPersonPositioning().accept(player, itemStack);
-			CompatibleWeaponRenderer.renderLeftArm(player, renderContext, (p, c) -> {
-				builder.getFirstPersonLeftHandPositioning().accept(c);
-			});
-			CompatibleWeaponRenderer.renderRightArm(player, renderContext, (p, c) -> {
-				builder.getFirstPersonRightHandPositioning().accept(c);
-			});
-			break;
-			default:
-		}
-
-		renderModelSource(renderContext, itemStack, transformType, null,  0.0F, 0.0f, -0.4f, 0.0f, 0.0f, 0.08f);
-
-		GL11.glPopMatrix();
-	}
-
-	protected void renderModelSource(RenderContext<RenderableState> renderContext, ItemStack itemStack, ItemCameraTransforms.TransformType transformType, Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
-
-
-		if(!(itemStack.getItem() instanceof ModelSource)) {
-			throw new IllegalArgumentException();
-		}
-
-		GL11.glPushMatrix();
-
-
-		ModelSource modelSource = (ModelSource)itemStack.getItem();
-
-		for(Tuple<ModelBase, String> texturedModel: modelSource.getTexturedModels()) {
-			mc.renderEngine.bindTexture(new ResourceLocation(ModReference.id + ":textures/models/" + texturedModel.getV()));
-			GL11.glPushMatrix();
-			GL11.glPushAttrib(GL11.GL_ENABLE_BIT);
-			ModelBase model = texturedModel.getU();
-
-			if(transformType != null) {
-				switch (transformType) {
-					case GROUND:
-						builder.getEntityModelPositioning().accept(model, itemStack);
-						break;
-					case GUI:
-						builder.getInventoryModelPositioning().accept(model, itemStack);
-						break;
-					case THIRD_PERSON_RIGHT_HAND: case THIRD_PERSON_LEFT_HAND:
-						builder.getThirdPersonModelPositioning().accept(model, itemStack);
-						break;
-					case FIRST_PERSON_RIGHT_HAND: case FIRST_PERSON_LEFT_HAND:
-						builder.getFirstPersonModelPositioning().accept(model, itemStack);
-						break;
-					default:
-				}
-			}
-
-			model.render(entity, f, f1, f2, f3, f4, f5);
-			GL11.glPopAttrib();
-			GL11.glPopMatrix();
-		}
-
-		@SuppressWarnings("unchecked")
-		CustomRenderer<RenderableState> postRenderer = (CustomRenderer<RenderableState>) modelSource.getPostRenderer();
-
-		if(postRenderer != null) {
-			renderContext.setAgeInTicks(-0.4f);
-			renderContext.setScale(0.08f);
-			renderContext.setCompatibleTransformType(transformType);
-
-			renderContext.setPlayerItemInstance(getModContext().getPlayerItemInstanceRegistry().getItemInstance(renderContext.getPlayer(), itemStack));
-
-			GL11.glPushMatrix();
-			GL11.glPushAttrib(GL11.GL_ENABLE_BIT | GL11.GL_CURRENT_BIT);
-
-			postRenderer.render(renderContext);
-			GL11.glPopAttrib();
-			GL11.glPopMatrix();
-		}
-		GL11.glPopMatrix();
-	}
 }
--- src/main/java/com/paneedah/weaponlib/compatibility/CompatibleWeaponRenderer.java
+++ src/main/java/com/paneedah/weaponlib/compatibility/CompatibleWeaponRenderer.java
@@ -1225,7 +1225,7 @@ public abstract class CompatibleWeaponRenderer extends ModelSourceRenderer imple
 		tessellator.draw();
 	}
 
-	public static <T> void renderRightArm(EntityLivingBase player, RenderContext<T> renderContext,
+	static <T> void renderRightArm(EntityLivingBase player, RenderContext<T> renderContext,
 			Positioner<Part, RenderContext<T>> positioner) {
 		
 		
@@ -1387,7 +1387,7 @@ public abstract class CompatibleWeaponRenderer extends ModelSourceRenderer imple
 	public static ModelRenderer bipedRightArm = null;
 	
 
-	public static <T> void renderLeftArm(EntityLivingBase player, RenderContext<T> renderContext,
+	static <T> void renderLeftArm(EntityLivingBase player, RenderContext<T> renderContext,
 			Positioner<Part, RenderContext<T>> positioner) {
 
 		//if(true) return;
