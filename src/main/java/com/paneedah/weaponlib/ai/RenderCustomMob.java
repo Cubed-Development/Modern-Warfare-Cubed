177accd05ed6f3e6c9667dc7741ca497e5561d28
Desoroxxx <desoroxxx@gmail.com>
2023-05-11 02:03:51 +0200

    Remove `CompatibleRenderBiped`

--- src/main/java/com/paneedah/weaponlib/ai/RenderCustomMob.java
+++ src/main/java/com/paneedah/weaponlib/ai/RenderCustomMob.java
@@ -1,15 +1,37 @@
 package com.paneedah.weaponlib.ai;
 
-import com.paneedah.weaponlib.compatibility.CompatibleRenderBiped;
+import com.paneedah.weaponlib.compatibility.CompatibleModelCustomMob;
 import net.minecraft.client.model.ModelBiped;
+import net.minecraft.client.renderer.entity.RenderBiped;
+import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
+import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.ResourceLocation;
 
-public class RenderCustomMob extends CompatibleRenderBiped<EntityCustomMob> {
+import static com.paneedah.mwc.proxies.ClientProxy.mc;
 
-    //private ResourceLocation texture;
+public class RenderCustomMob<T extends EntityLiving> extends RenderBiped<T> {
 
     public RenderCustomMob(ModelBiped model) {
-        super(model, 0.5F);
+        super(mc.getRenderManager(), model, 0.5F);
+
+        this.addLayer(new LayerHeldItem(this));
+        this.addLayer(new LayerBipedArmor(this) {
+            protected void initArmor() {
+                this.modelLeggings = new CompatibleModelCustomMob(0.5F, true);
+                this.modelArmor = new CompatibleModelCustomMob(1.0F, true);
+            }
+        });
+    }
+
+    @Override
+    public void doRender(T entity, double x, double y, double z, float entityYaw, float partialTicks) {
+        EntityCustomMob entityCustomMob = (EntityCustomMob) entity;
+
+        EntityConfiguration configuration = entityCustomMob.getConfiguration();
+        this.mainModel = configuration.getTexturedModelVariants().get(entityCustomMob.getVariant()).model;
+
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
     }
 
     /**
@@ -17,14 +39,10 @@ public class RenderCustomMob extends CompatibleRenderBiped<EntityCustomMob> {
      * unless you call Render.bindEntityTexture.
      */
     @Override
-    protected ResourceLocation getCompatibleEntityTexture(EntityCustomMob entity) {
-        EntityConfiguration configuration = entity.getConfiguration();
-        return configuration.getTexturedModelVariants().get(entity.getVariant()).textureResource;
-    }
-    
-    @Override
-    public void prepareCompatibleRender(EntityCustomMob entity, double x, double y, double z, float entityYaw, float partialTicks) {
-        EntityConfiguration configuration = entity.getConfiguration();
-        this.mainModel = configuration.getTexturedModelVariants().get(entity.getVariant()).model;
+    protected ResourceLocation getEntityTexture(T entity) {
+        EntityCustomMob entityCustomMob = (EntityCustomMob) entity;
+
+        EntityConfiguration configuration = entityCustomMob.getConfiguration();
+        return configuration.getTexturedModelVariants().get(entityCustomMob.getVariant()).textureResource;
     }
 }
