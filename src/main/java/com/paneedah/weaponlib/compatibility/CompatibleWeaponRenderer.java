788754fa3956af01e25407699c23f3ded970e9e2
Desoroxxx <desoroxxx@gmail.com>
2023-05-12 21:43:18 +0200

    Remove `CompatibleGrenadeRenderer`

--- src/main/java/com/paneedah/weaponlib/compatibility/CompatibleWeaponRenderer.java
+++ src/main/java/com/paneedah/weaponlib/compatibility/CompatibleWeaponRenderer.java
@@ -1030,7 +1030,7 @@ public abstract class CompatibleWeaponRenderer extends ModelSourceRenderer imple
 	public static final ModelRenderer bipedLeftArm = null;
 	
 	
-	static void fixVersionSpecificFirstPersonPositioning(TransformType transformType) {
+	public static void fixVersionSpecificFirstPersonPositioning(TransformType transformType) {
 		int i = transformType == TransformType.FIRST_PERSON_RIGHT_HAND ? 1 : -1;
 
 		GL11.glTranslatef(0.5f, 0.5f, 0.5f); // untranslate 1.9.4
@@ -1225,7 +1225,7 @@ public abstract class CompatibleWeaponRenderer extends ModelSourceRenderer imple
 		tessellator.draw();
 	}
 
-	static <T> void renderRightArm(EntityLivingBase player, RenderContext<T> renderContext,
+	public static <T> void renderRightArm(EntityLivingBase player, RenderContext<T> renderContext,
 			Positioner<Part, RenderContext<T>> positioner) {
 		
 		
@@ -1387,7 +1387,7 @@ public abstract class CompatibleWeaponRenderer extends ModelSourceRenderer imple
 	public static ModelRenderer bipedRightArm = null;
 	
 
-	static <T> void renderLeftArm(EntityLivingBase player, RenderContext<T> renderContext,
+	public static <T> void renderLeftArm(EntityLivingBase player, RenderContext<T> renderContext,
 			Positioner<Part, RenderContext<T>> positioner) {
 
 		//if(true) return;
--- src/main/java/com/paneedah/weaponlib/grenade/GrenadeRenderer.java
+++ src/main/java/com/paneedah/weaponlib/grenade/GrenadeRenderer.java
@@ -5,13 +5,28 @@ import com.paneedah.weaponlib.*;
 import com.paneedah.weaponlib.animation.*;
 import com.paneedah.weaponlib.animation.DebugPositioner.TransitionConfiguration;
 import com.paneedah.weaponlib.animation.MultipartPositioning.Positioner;
-import com.paneedah.weaponlib.compatibility.CompatibleGrenadeRenderer;
+import com.paneedah.weaponlib.compatibility.CompatibleWeaponRenderer;
+import com.paneedah.weaponlib.compatibility.ModelSourceRenderer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.block.model.*;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.commons.lang3.tuple.Pair;
 import org.lwjgl.opengl.GL11;
 
+import javax.vecmath.Matrix4f;
 import java.util.*;
 import java.util.Map.Entry;
 import java.util.function.Consumer;
@@ -22,7 +37,7 @@ import static com.paneedah.mwc.proxies.ClientProxy.mc;
 import static com.paneedah.mwc.utils.ModReference.log;
 import static com.paneedah.weaponlib.compatibility.CompatibilityProvider.compatibility;
 
-public class GrenadeRenderer extends CompatibleGrenadeRenderer {
+public class GrenadeRenderer extends ModelSourceRenderer implements IBakedModel {
 
 	private static final float DEFAULT_RANDOMIZING_RATE = 0.33f;
 
@@ -30,6 +45,43 @@ public class GrenadeRenderer extends CompatibleGrenadeRenderer {
 
 	private static final int DEFAULT_ANIMATION_DURATION = 70;
 
+	protected EntityLivingBase player;
+
+	private ItemOverrideList itemOverrideList = new WeaponItemOverrideList(Collections.emptyList());
+
+	protected ItemStack itemStack;
+	ItemCameraTransforms.TransformType transformType;
+
+	protected TextureManager textureManager;
+
+	private Pair<? extends IBakedModel, Matrix4f> pair;
+
+	private class WeaponItemOverrideList extends ItemOverrideList {
+
+		public WeaponItemOverrideList(List<ItemOverride> overridesIn) {
+			super(overridesIn);
+		}
+
+		@Override
+		public IBakedModel handleItemState(IBakedModel originalModel, ItemStack stack, World world, EntityLivingBase entity) {
+			GrenadeRenderer.this.itemStack = stack;
+			GrenadeRenderer.this.player = entity;
+			return super.handleItemState(originalModel, stack, world, entity);
+		}
+	}
+
+	protected static class StateDescriptor {
+		protected MultipartRenderStateManager<RenderableState, Part, RenderContext<RenderableState>> stateManager;
+		protected float rate;
+		protected float amplitude = 0.04f;
+		private PlayerGrenadeInstance instance;
+		public StateDescriptor(PlayerGrenadeInstance instance, MultipartRenderStateManager<RenderableState, Part, RenderContext<RenderableState>> stateManager, float rate, float amplitude) {
+			this.instance = instance;
+			this.stateManager = stateManager;
+			this.rate = rate;
+			this.amplitude = amplitude;
+		}
+	}
 
 	private static class SimplePositioning {
 	    Part attachedTo;
@@ -530,7 +582,9 @@ public class GrenadeRenderer extends CompatibleGrenadeRenderer {
 	protected ClientModContext clientModContext;
 
 	private GrenadeRenderer(Builder builder) {
-		super(builder);
+		this.builder = builder;
+		this.textureManager = mc.getTextureManager();
+		this.pair = Pair.of((IBakedModel) this, null);
 		this.builder = builder;
 		this.firstPersonStateManagers = new HashMap<>();
 		this.weaponTransitionProvider = new WeaponPositionProvider();
@@ -590,7 +644,6 @@ public class GrenadeRenderer extends CompatibleGrenadeRenderer {
 
 	}
 
-	@Override
 	protected StateDescriptor getStateDescriptor(EntityLivingBase player, ItemStack itemStack) {
 		float amplitude = builder.normalRandomizingAmplitude;
 		float rate = builder.normalRandomizingRate;
@@ -808,7 +861,6 @@ public class GrenadeRenderer extends CompatibleGrenadeRenderer {
 		}
 	}
 
-	@Override
 	public void renderItem(ItemStack weaponItemStack, RenderContext<RenderableState> renderContext,
 			Positioner<Part, RenderContext<RenderableState>> positioner) {
 
@@ -846,7 +898,6 @@ public class GrenadeRenderer extends CompatibleGrenadeRenderer {
         renderAttachments(positioner, renderContext, attachments);
 	}
 
-	@Override
 	public void renderAttachments(Positioner<Part, RenderContext<RenderableState>> positioner, RenderContext<RenderableState> renderContext,List<CompatibleAttachment<? extends AttachmentContainer>> attachments) {
 		for(CompatibleAttachment<?> compatibleAttachment: attachments) {
 			if(compatibleAttachment != null && !(compatibleAttachment.getAttachment() instanceof ItemSkin)) {
@@ -945,4 +996,181 @@ public class GrenadeRenderer extends CompatibleGrenadeRenderer {
         return builder.thrownEntityPositioning;
     }
 
+	@Override
+	public List<BakedQuad> getQuads(IBlockState state, EnumFacing side, long rand) {
+		// Todo: Actually make rendering compatible with Emissive Renderer
+		if (net.minecraftforge.common.ForgeModContainer.allowEmissiveItems) {
+			return Collections.emptyList();
+		}
+
+		if(itemStack == null) return Collections.emptyList();
+		if(transformType == ItemCameraTransforms.TransformType.GROUND
+				|| transformType == ItemCameraTransforms.TransformType.GUI
+				|| transformType == ItemCameraTransforms.TransformType.FIRST_PERSON_RIGHT_HAND
+				|| transformType == ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND
+		) {
+
+			Tessellator tessellator = Tessellator.getInstance();
+			BufferBuilder worldrenderer = tessellator.getBuffer();
+			tessellator.draw();
+			GlStateManager.pushMatrix();
+
+			if (player != null) {
+				if (transformType == ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND) {
+					if (player.isSneaking()) GlStateManager.translate(0.0F, -0.2F, 0.0F);
+				}
+			}
+
+			if (onGround()) {
+				GlStateManager.scale(-3f, -3f, -3f);
+			}
+
+			int currentTextureId = GlStateManager.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
+			renderItem();
+			if(currentTextureId != 0) {
+				GlStateManager.bindTexture(currentTextureId);
+			}
+			GlStateManager.popMatrix();
+			worldrenderer.begin(GL11.GL_QUADS, DefaultVertexFormats.ITEM);
+		}
+
+		// Reset the dynamic values.
+		this.player = null;
+		this.itemStack = null;
+		this.transformType = null;
+
+		return Collections.emptyList();
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void renderItem() {
+		GL11.glPushMatrix();
+
+		RenderContext<RenderableState> renderContext = new RenderContext<>(getClientModContext(), player, itemStack);
+
+		//float limbSwing, float flimbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale
+		//0.0F, 0.0f, -0.4f, 0.0f, 0.0f, 0.08f);
+		renderContext.setAgeInTicks(-0.4f);
+		renderContext.setScale(0.08f);
+		renderContext.setCompatibleTransformType(transformType);
+
+		Positioner<Part, RenderContext<RenderableState>> positioner = null;
+		switch (transformType)
+		{
+			case GROUND:
+				GL11.glScaled(-1F, -1F, 1F);
+				GL11.glScaled(0.35F, 0.35F, 0.35F);
+				GL11.glTranslatef(-0.7f, -1f, -0.1f);
+				GL11.glRotatef(0F, 1f, 0f, 0f);
+				//GL11.glRotatef(150F, 0f, 1f, 0f);
+				GL11.glRotatef(90F, 0f, 0f, 1f);
+				builder.getEntityPositioning().accept(itemStack);
+				break;
+
+			case GUI:
+				GL11.glScaled(-1F, -1F, 1F);
+				GL11.glScaled(0.6F, 0.6F, 0.6F);
+				GL11.glTranslatef(-0.7f, -0.8f, -0.1f);
+				GL11.glRotatef(-30F, 1f, 0f, 0f);
+				GL11.glRotatef(40F, 0f, 1f, 0f);
+				GL11.glRotatef(0F, 0f, 0f, 1f);
+				builder.getInventoryPositioning().accept(itemStack);
+				break;
+
+			case THIRD_PERSON_RIGHT_HAND: case THIRD_PERSON_LEFT_HAND:
+			GL11.glScaled(-1F, -1F, 1F);
+			GL11.glScaled(0.4F, 0.4F, 0.4F);
+			GL11.glTranslatef(-1.5f, -2.4f, 1.3f);
+			GL11.glRotatef(-100F, 1f, 0f, 0f);
+			GL11.glRotatef(50F, 0f, 1f, 0f);
+			GL11.glRotatef(0F, 0f, 0f, 1f);
+			builder.getThirdPersonPositioning().accept(renderContext);
+			break;
+
+			case FIRST_PERSON_RIGHT_HAND: case FIRST_PERSON_LEFT_HAND:
+
+
+			CompatibleWeaponRenderer.fixVersionSpecificFirstPersonPositioning(transformType);
+
+			GL11.glScaled(-1F, -1F, 1F);
+
+			StateDescriptor stateDescriptor = getStateDescriptor(player, itemStack);
+
+			renderContext.setPlayerItemInstance(stateDescriptor.instance);
+
+			MultipartPositioning<Part, RenderContext<RenderableState>> multipartPositioning = stateDescriptor.stateManager.nextPositioning();
+
+			renderContext.setTransitionProgress(multipartPositioning.getProgress());
+
+			renderContext.setFromState(multipartPositioning.getFromState(RenderableState.class));
+
+			renderContext.setToState(multipartPositioning.getToState(RenderableState.class));
+
+			positioner = multipartPositioning.getPositioner();
+
+			renderContext.capturePartPosition(Part.NONE);
+
+			CompatibleWeaponRenderer.renderLeftArm(player, renderContext, positioner);
+
+			// Randomization is supported for the right hand only only
+			positioner.randomize(stateDescriptor.rate, stateDescriptor.amplitude);
+
+			CompatibleWeaponRenderer.renderRightArm(player, renderContext, positioner);
+
+			positioner.position(Part.MAIN_ITEM, renderContext);
+
+			if(DebugPositioner.isDebugModeEnabled()) {
+				DebugPositioner.position(Part.MAIN_ITEM, renderContext);
+			}
+
+			renderContext.capturePartPosition(Part.MAIN_ITEM);
+
+			break;
+			default:
+		}
+
+		renderItem(itemStack, renderContext, positioner);
+
+		GL11.glPopMatrix();
+	}
+
+	protected boolean onGround() {
+		return transformType == null;
+	}
+
+	@Override
+	public final boolean isAmbientOcclusion() {
+		return true;
+	}
+
+	@Override
+	public final boolean isGui3d() {
+		return true;
+	}
+
+	@Override
+	public final boolean isBuiltInRenderer() {
+		return false;
+	}
+
+	@Override
+	public TextureAtlasSprite getParticleTexture() {
+		return mc.getTextureMapBlocks().getMissingSprite();
+	}
+
+	@Override
+	public ItemCameraTransforms getItemCameraTransforms() {
+		return ItemCameraTransforms.DEFAULT;
+	}
+
+	@Override
+	public ItemOverrideList getOverrides() {
+		return itemOverrideList;
+	}
+
+	@Override
+	public Pair<? extends IBakedModel, Matrix4f> handlePerspective(ItemCameraTransforms.TransformType cameraTransformType) {
+		this.transformType = cameraTransformType;
+		return pair;
+	}
 }
