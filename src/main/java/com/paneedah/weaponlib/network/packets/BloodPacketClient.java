7d8681956ea6eb917a84dca7941491d197e2b206
Desoroxxx <desoroxxx@gmail.com>
2023-05-13 15:36:46 +0200

    Revert "Remove `addBloodParticle` from `Compatibility`"
    This reverts commit fa4afc4da75c9ce1285924686f762bb6595effd7.

--- src/main/java/com/paneedah/weaponlib/network/packets/BloodPacketClient.java
+++ src/main/java/com/paneedah/weaponlib/network/packets/BloodPacketClient.java
@@ -3,13 +3,11 @@ package com.paneedah.weaponlib.network.packets;
 import com.paneedah.weaponlib.ModContext;
 import com.paneedah.weaponlib.compatibility.CompatibleMessageHandler;
 import com.paneedah.weaponlib.jim.util.RandomUtil;
-import com.paneedah.weaponlib.particle.ParticleBlood;
 import io.netty.buffer.ByteBuf;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.relauncher.Side;
 
-import static com.paneedah.mwc.proxies.ClientProxy.mc;
 import static com.paneedah.weaponlib.compatibility.CompatibilityProvider.compatibility;
 
 public class BloodPacketClient implements net.minecraftforge.fml.common.network.simpleimpl.IMessage {
@@ -66,25 +64,32 @@ public class BloodPacketClient implements net.minecraftforge.fml.common.network.
 
 		@Override
 		public <T extends net.minecraftforge.fml.common.network.simpleimpl.IMessage> T onCompatibleMessage(BloodPacketClient message, MessageContext messageContext) {
-			if(messageContext.side == Side.CLIENT) {
-				compatibility.runInMainClientThread(() -> {
-					double velX = message.velx;
-					double velY = message.vely;
-					double velZ = message.velz;
-
-					double length = Math.sqrt(velX*velX + velY*velY + velZ*velZ);
-					velX /= -length;
-					velY /= -length;
-					velZ /= -length;
-
-					double scale = 0.2;
-					double spreader = 0.05;
-
-					RandomUtil util = new RandomUtil();
-
-					for(int i = 0; i < 15; ++i) {
-						mc.effectRenderer.addEffect(new ParticleBlood(mc.world, message.x, message.y, message.z, velX*scale + util.getRandomWithNegatives(spreader), velY*scale + util.getRandomWithNegatives(spreader), velZ*scale + util.getRandomWithNegatives(spreader)));
-					}
+			 if(messageContext.side == Side.CLIENT) {
+		            compatibility.runInMainClientThread(() -> {
+					
+		            	
+		            	double velX = message.velx;
+		            	double velY = message.vely;
+		            	double velZ = message.velz;
+		            	
+		            	double length = Math.sqrt(velX*velX + velY*velY + velZ*velZ);
+		            	velX /= -length;
+		            	velY /= -length;
+		            	velZ /= -length;
+		            	
+		            	
+		            	double scale = 0.2;
+		            	double spreader = 0.05;
+		            	
+		            	RandomUtil util = new RandomUtil();
+		            	
+		            	for(int i = 0; i < 15; ++i) {
+		            	///	System.out.println(m.x + " | " + m.y  + " | " + m.z);
+		            		compatibility.addBloodParticle(modContext, message.x, message.y, message.z, velX*scale + util.getRandomWithNegatives(spreader), velY*scale + util.getRandomWithNegatives(spreader), velZ*scale + util.getRandomWithNegatives(spreader));
+			            	
+							
+		            	}
+		            	
 				});
 			}
 			
