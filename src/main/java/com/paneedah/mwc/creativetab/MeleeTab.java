0f81841c644e8cdc32f805f0309416b8228620f0
strubium <113206902+strubium@users.noreply.github.com>
2023-05-15 10:44:27 -0500

    Make Tab Work

--- src/main/java/com/paneedah/mwc/creativetab/MeleeTab.java
+++ src/main/java/com/paneedah/mwc/creativetab/MeleeTab.java
@@ -1,1 +1,20 @@
+package com.paneedah.mwc.creativetab;
 
+import com.paneedah.mwc.weapons.Guns;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class MeleeTab extends CreativeTabs {
+
+	      public MeleeTab(int par1, String par2Str) {
+		            super(par1, par2Str);
+	      }
+
+	      @Override
+	      @SideOnly(Side.CLIENT)
+	      public ItemStack createIcon() {
+		            return new ItemStack(Guns.AK47);
+	      }
+}
