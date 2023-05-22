9889417e496086bc183b72a8a849cc6ec6f565d6
Desoroxxx <desoroxxx@gmail.com>
2023-05-22 03:52:45 +0200

    Hardcode the compatibility provider once again

--- src/main/java/com/paneedah/weaponlib/compatibility/CompatibilityProvider.java
+++ src/main/java/com/paneedah/weaponlib/compatibility/CompatibilityProvider.java
@@ -1,19 +1,7 @@
 package com.paneedah.weaponlib.compatibility;
 
-import net.minecraftforge.common.ForgeVersion;
-
 @Deprecated
 public final class CompatibilityProvider {
 	
-	public static Compatibility compatibility = initCompatibility();
-
-	private static Compatibility initCompatibility() {
-		String compatibilityClassName = Compatibility.class.getName() + ForgeVersion.mcVersion.replace('.', '_');
-		
-		try {
-			return (Compatibility) Class.forName(compatibilityClassName).newInstance();
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-			throw new IllegalStateException("Cannot find compatible implementation class " + compatibilityClassName);
-		}
-	}
+	public static Compatibility compatibility = new Compatibility1_12_2();
 }
