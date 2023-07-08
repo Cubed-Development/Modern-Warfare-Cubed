12d4222c44a544b338155ca26b0eb49e323bb22c
Koud-Wind <cold1950963108@outlook.com>
2023-07-08 17:09:01 +0800

    Squashed commit of the following:
    commit 1ba8df0ffb0bebf0aabc749578c12164d351a508
    Author: Koud-Wind <123817318+Koud-Wind@users.noreply.github.com>
    Date:   Tue Jul 4 13:54:33 2023 +0800
    
        Update Changelog.md
    
    commit 77f9dec57d9b7f95e29a59c9faf446cb83a46b60
    Author: Koud-Wind <123817318+Koud-Wind@users.noreply.github.com>
    Date:   Tue Jul 4 00:52:10 2023 +0800
    
        Update Changelog.md
    
    commit 569b23ff06984e6418d9a19a09525387c9337fe5
    Author: Koud-Wind <cold1950963108@outlook.com>
    Date:   Mon Jun 26 11:50:31 2023 +0800
    
        正式修复

--- src/main/java/com/paneedah/mwc/entities/Entities.java
+++ src/main/java/com/paneedah/mwc/entities/Entities.java
@@ -1,6 +1,6 @@
 package com.paneedah.mwc.entities;
 
-import com.paneedah.mwc.MWC;
+import com.paneedah.mwc.ModernWarfareMod;
 import com.paneedah.mwc.proxies.CommonProxy;
 import com.paneedah.mwc.weapons.Guns;
 import com.paneedah.mwc.weapons.Magazines;
@@ -19,17 +19,70 @@ public class Entities {
 
     public static void init(CommonProxy commonProxy) {
 
-        new BanditEntityFactory().createEntity(MWC.modContext);
+        new EntityConfiguration.Builder()
+        .withName("terrorist")
+        .withBaseClass(EntityCustomMob.class)
+        .withMaxHealth(40)
+        .withEntityIdSupplier(() -> 10000)
+        .withEquipmentOption(Guns.VSSVintorez, EnumDifficulty.EASY, 0.1f, Magazines.VSSVintorezMag)
+        .withEquipmentOption(Guns.VSSVintorez, EnumDifficulty.HARD, 0.1f, Magazines.ASValMag)
+        .withEquipmentOption(Guns.MakarovPM, EnumDifficulty.EASY, 0.1f, Magazines.MakarovMag)
+        .withEquipmentOption(Guns.Remington870, EnumDifficulty.EASY, 0.07f)
+        .withPrimaryEquipmentDropChance(0.4f)
+        .withSpawn(10, 0, 15, BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.HILLS)
+        .withMaxTolerableLightBrightness(1f)
+        .withSpawnEgg(0xFF808000, 0xFF556B2F)
+        .withTexturedModelVariant("com.paneedah.mwc.models.BanditNew", "banditnew.png")
+        .withTexturedModelVariant("com.paneedah.mwc.models.BanditNew2", "banditnew2.png")
+        .withTexturedModelVariant("com.paneedah.mwc.models.BanditNew3", "banditnew3.png")
+        .withTexturedModelVariant("com.paneedah.mwc.models.BanditNew4", "banditnew4.png")
+        .withTexturedModelVariant("com.paneedah.mwc.models.BanditNew5", "banditnew5.png")
+        .withHurtSound("hurt")
+        .withAmbientSound("drawweapon")
+//      .withStepSound("step")
+        .withAiTask(1, e -> new EntityAISwimming(e))
+        .withAiTask(3, e -> new EntityAIAvoidEntity<>((EntityCreature) e, EntityWolf.class, 6.0F, 1.0D, 1.2D))
+        .withAiTask(4, e -> new EntityAIAttackRangedWeapon((EntityCustomMob)e, 1.0D, 10, 30.0F))
+        .withAiTask(5, e -> new EntityAIWander((EntityCreature)e, 1.0D))
+        .withAiTask(6, e -> new EntityAIWatchClosest(e, EntityPlayer.class, 50.0F))
+        .withAiTask(6, e -> new EntityAILookIdle(e))
+//      .withAiTask(7, e -> new EntityAIBreakDoor(e))
+
+        .withAiTargetTask(1, e -> new EntityAIHurtByTarget((EntityCreature)e, false))
+        .withAiTargetTask(2, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityPlayer.class, true))
+        .withAiTargetTask(2, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "soldier", true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityVillager.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityZombie.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityHusk.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntitySkeleton.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntitySpider.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityEnderman.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityCreeper.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntitySlime.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityDragon.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityEndermite.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityBlaze.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityGhast.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityIllusionIllager.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityMagmaCube.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityPigZombie.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntitySpellcasterIllager.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityStray.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityVindicator.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityWitch.class, true))
+        .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityZombieVillager.class, true))
+        .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "tyke", true))
+        .register(ModernWarfareMod.MOD_CONTEXT);
         
         new EntityConfiguration.Builder()
         .withName("soldier")
         .withBaseClass(EntityCustomMob.class)
-        .withMaxHealth(80)
+        .withMaxHealth(40)
         .withEntityIdSupplier(() -> 10011)
         .withEquipmentOption(Guns.M4A1, EnumDifficulty.EASY, 0.1f, Magazines.M4A1Mag)
         .withEquipmentOption(Guns.M110, EnumDifficulty.EASY, 0.1f, Magazines.M110Mag)
         .withPrimaryEquipmentDropChance(0.4f)
-        .withSpawn(0, 5, 6, BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.FOREST, BiomeDictionary.Type.HILLS)
+        .withSpawn(10, 0, 15, BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.HILLS)
         .withMaxTolerableLightBrightness(1f)
         .withSpawnEgg(0x5A674F, 0x464039)
         .withTexturedModelVariant("com.paneedah.mwc.models.Soldier", "soldier.png")
@@ -70,7 +123,7 @@ public class Entities {
         .withAiTargetTask(3, e -> new EntityAINearestAttackableTarget<>((EntityCreature) e, EntityZombieVillager.class, true))
         .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "terrorist", true))
         .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "tyke", true))
-        .register(MWC.modContext);
+        .register(ModernWarfareMod.MOD_CONTEXT);
 
         new EntityConfiguration.Builder()
                 .withName("turret")
@@ -120,7 +173,7 @@ public class Entities {
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "soldier", true))
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "terrorist", true))
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "tyke", true))
-                .register(MWC.modContext);
+                .register(ModernWarfareMod.MOD_CONTEXT);
         new EntityConfiguration.Builder()
                 .withName("turretupgraded")
                 .withBaseClass(EntityCustomMob.class)
@@ -169,7 +222,7 @@ public class Entities {
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "soldier", true))
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "terrorist", true))
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "tyke", true))
-                .register(MWC.modContext);
+                .register(ModernWarfareMod.MOD_CONTEXT);
 
         new EntityConfiguration.Builder()
                 .withName("turretsilenced")
@@ -219,6 +272,6 @@ public class Entities {
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "soldier", true))
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "terrorist", true))
                 .withAiTargetTask(4, e -> new BetterAINearestAttackableTarget<>((EntityCreature) e, EntityCustomMob.class, "tyke", true))
-                .register(MWC.modContext);
+                .register(ModernWarfareMod.MOD_CONTEXT);
         }
 }
--- src/main/java/com/paneedah/weaponlib/config/ModernConfigManager.java
+++ src/main/java/com/paneedah/weaponlib/config/ModernConfigManager.java
@@ -151,24 +151,24 @@ public class ModernConfigManager {
 	public static String statusBarPosition = "TOP_RIGHT";
 
 
-	@RangeDouble(min=0.1, max=2.0)
+	@RangeDouble(min=0.1, max=5.0)
 	@ConfigSync(category = CATEGORY_ENTITIES, comment = "How much health should terrorists have?")
 	public static double terroristHealth = 1.0;
 
-	@RangeDouble(min=0, max=1.0)
+	@RangeDouble(min=0, max=2.0)
 	@ConfigSync(category = CATEGORY_ENTITIES, comment = "How much should terrorists spawn?")
-	public static double terroristsSpawn = 1.0;
+	public static double terroristsSpawn = 0.2;
 
 	@ConfigSync(category = CATEGORY_ENTITIES, comment = "What equipment should the terrorists have?")
 	public static String terroristEquipmentConfiguration = "m4a1:1.0:M4A1Mag_2, m16a1:1.0:M4A1Mag_2, Remington870:1.0";
 
-	@RangeDouble(min=0, max=1.0)
+	@RangeDouble(min=0, max=2.0)
 	@ConfigSync(category = CATEGORY_ENTITIES, comment = "How much should soldiers spawn?")
-	public static double soldierspawn = 0.0;
+	public static double soldierSpawn = 0.0;
 
-	@RangeDouble(min=0.1, max=2.0)
+	@RangeDouble(min=0.1, max=5.0)
 	@ConfigSync(category = CATEGORY_ENTITIES, comment = "How much health should soldiers have?")
-	public static double soldierHealth = 1.0;
+	public static double soldierHealth = 2.0;
 
 	private static Configuration config = null;
 
@@ -259,11 +259,6 @@ public class ModernConfigManager {
 	public static void saveConfig() {
 		config.save();
 	}
-
-	public static final Map<String, AIEntity> aiEntities = new LinkedHashMap<String, AIEntity>(){{
-		put("terrorist", new AIEntity("terrorist", ModernConfigManager.terroristHealth, ModernConfigManager.terroristsSpawn));
-		put("soldier", new AIEntity("soldier", ModernConfigManager.soldierHealth, ModernConfigManager.soldierspawn));
-	}};
 	
 	public static void init() {
 		if(isLoaded)
