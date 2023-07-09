85c39a5c53629e63820f9cfdab70eff3729d26cf
Paneedah <paneedah.github@panada.cc>
2023-07-09 22:12:04 +0100

    Add missing end of shoot sound for AK101

--- src/main/java/com/paneedah/mwc/items/guns/AK101Factory.java
+++ src/main/java/com/paneedah/mwc/items/guns/AK101Factory.java
@@ -43,6 +43,7 @@ public class AK101Factory {
         .withUnloadSound("ak_unload")
         .withInspectSound("inspection")
         .withDrawSound("ak_draw")
+        .withEndOfShootSound("gun_click")
         .withReloadingTime(45)
         .withCrosshair("gun")
         .withCrosshairRunning("Running")
