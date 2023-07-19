37c3d9a90d16183e107bf3375fcd65812a9264ec
strubium <113206902+strubium@users.noreply.github.com>
2023-07-19 15:01:36 -0500

    White Desk Middle

--- src/main/java/com/paneedah/mwc/tileentities/TileEntities.java
+++ src/main/java/com/paneedah/mwc/tileentities/TileEntities.java
@@ -1190,6 +1190,28 @@ public class TileEntities {
         .withModelClassName("com.paneedah.mwc.models.WhiteDeskMiddle")
         .withTextureName("textures/models/shelf.png")
         .withCreativeTab(MWC.PROPS_TAB)
+        .withBoundingBox(
+        		blockState -> {
+        			AxisAlignedBB boundingBox = null;
+        			EnumFacing facing = blockState.getValue(CustomTileEntityBlock.FACING);
+        			switch(facing) {
+        			case WEST:
+        				boundingBox = new AxisAlignedBB(0, .5, 0, 1, 1, 1);
+        				break;
+        			case EAST:
+        				boundingBox = new AxisAlignedBB(0, .5, 0, 1, 1, 1);
+        				break;
+        			case NORTH:
+        				boundingBox = new AxisAlignedBB(0, .5, 0, 1, 1, 1);
+        				break;
+        			case SOUTH:
+        				boundingBox = new AxisAlignedBB(0, .5, 0, 1, 1, 1);
+        				break;
+        			default:
+        			}
+        			return boundingBox;
+        		}
+        )
         .withPositioning(tileEntity -> {
             GL11.glScalef(1f, 1f, 1f);
             GL11.glTranslatef(0.5f, 0f, 0.5f);
