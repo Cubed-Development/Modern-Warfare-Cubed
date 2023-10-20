aab57d97a7902c164c5bc5c51edf882044841633
Braiam23 <133471454+Braiam23@users.noreply.github.com>
2023-10-19 21:39:17 -0500

    Update M79Factory.java

--- src/main/java/com/paneedah/mwc/items/guns/M79Factory.java
+++ src/main/java/com/paneedah/mwc/items/guns/M79Factory.java
@@ -73,7 +73,7 @@ public class M79Factory implements GunFactory {
 
             .withModel(new M79())
             .withEntityPositioning(itemStack -> {
-                    GL11.glScaled(0.35F, 0.35F, 0.35F);
+                    GL11.glScaled(0.3F, 0.3F, 0.3F);
                     GL11.glRotatef(-90F, 0f, 0f, 4f);
             })
             .withInventoryPositioning(itemStack -> {
@@ -82,7 +82,7 @@ public class M79Factory implements GunFactory {
                     GL11.glRotatef(-120F, -0.5f, 7f, 3f);
             })
             .withThirdPersonPositioning((renderContext) -> {
-                    GL11.glScaled(0.6F, 0.6F, 0.6F);
+                    GL11.glScaled(0.3F, 0.3F, 0.3F);
                     GL11.glTranslatef(-1.7F, -0.8F, 1.9F);
                     GL11.glRotatef(-45F, 0f, 1f, 0f);
                     GL11.glRotatef(70F, 1f, 0f, 0f);
