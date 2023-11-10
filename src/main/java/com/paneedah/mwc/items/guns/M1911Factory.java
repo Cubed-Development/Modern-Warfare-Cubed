f3738626856e889d7eb5d51f046176aa6914fe1c
Braiam23 <133471454+Braiam23@users.noreply.github.com>
2023-11-09 20:23:26 -0500

    Update M1911Factory.java

--- src/main/java/com/paneedah/mwc/items/guns/M1911Factory.java
+++ src/main/java/com/paneedah/mwc/items/guns/M1911Factory.java
@@ -151,7 +151,7 @@ public class M1911Factory implements GunFactory {
                                         .withPosition(-0.945000f, 4.065000f, -7.845000f)
                                         .withRotation(0.000000f, 1.000000f, 10.681469f)
                                         .withPivotPoint(-0.12000000357627871F, -0.36000001072883614F, 0.040000001192092904F)
-                                        .withScale(2.5F, 2.5F, 2.5F)
+                                        .withScale(2.8F, 2.8F, 2.8F)
                         )
 
                         .withFirstPersonHandPositioning(
