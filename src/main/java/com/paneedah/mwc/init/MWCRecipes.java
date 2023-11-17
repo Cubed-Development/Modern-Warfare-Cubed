c4fd040377be723e03d26cb759c0d12c4f9e6fe7
yor42 <49832212+yor42@users.noreply.github.com>
2023-11-18 00:22:51 +0900

    move Recipe Registry to its own event as Brachy suggested

--- src/main/java/com/paneedah/mwc/init/MWCRecipes.java
+++ src/main/java/com/paneedah/mwc/init/MWCRecipes.java
@@ -1,9 +1,17 @@
 package com.paneedah.mwc.init;
 
+import com.paneedah.weaponlib.crafting.CraftingFileManager;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
+import static com.paneedah.mwc.utils.ModReference.ID;
+
+@Mod.EventBusSubscriber(modid =ID)
 public class MWCRecipes {
     public static void register() {
         // Smelting Recipes not supported by JSON.
@@ -15,5 +23,10 @@ public class MWCRecipes {
         GameRegistry.addSmelting(MWCItems.steelDust, new ItemStack(MWCItems.steelIngot), 1f);
         GameRegistry.addSmelting(MWCItems.sulfurDust, new ItemStack(Items.GUNPOWDER), 0.7f);
         GameRegistry.addSmelting(Items.COAL, new ItemStack(MWCItems.graphiteChunk), 2f);
+    }
+
+    @SubscribeEvent
+    public static void registerItems(RegistryEvent.Register<IRecipe> itemRegistryEvent) {
+        CraftingFileManager.getInstance().loadDirectory();
     }
 }
