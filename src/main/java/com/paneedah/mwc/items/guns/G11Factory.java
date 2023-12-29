108bb0131bb87a02c5bd27ce40f0e4ec6d0be319
Braiam23 <133471454+Braiam23@users.noreply.github.com>
2023-12-29 15:19:24 -0500

    Update G11Factory.java

--- src/main/java/com/paneedah/mwc/items/guns/G11Factory.java
+++ src/main/java/com/paneedah/mwc/items/guns/G11Factory.java
@@ -201,7 +201,7 @@ public class G11Factory {
                         .withEntityPositioning(itemStack -> {
                             GL11.glScaled(0.35F, 0.35F, 0.35F);
                             GL11.glTranslatef(0, 0f, 3f);
-                            GL11.glRotatef(0F, 0f, 0f, 45f);
+                            GL11.glRotatef(0F, 0f, 0f, 4f);
                         })
                         .withInventoryPositioning(itemStack -> {
                             GL11.glScaled(0.28F, 0.28F, 0.28F);
