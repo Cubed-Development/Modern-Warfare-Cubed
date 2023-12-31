511f364171cedd7eca659556c792772aaf5f680c
Koud-Wind <cold1950963108@outlook.com>
2024-01-01 02:55:56 +0800

    Customize damage types and eliminate knockback deceleration

--- src/main/java/com/paneedah/weaponlib/compatibility/ServerInterceptors.java
+++ src/main/java/com/paneedah/weaponlib/compatibility/ServerInterceptors.java
@@ -2,18 +2,19 @@ package com.paneedah.weaponlib.compatibility;
 
 import com.paneedah.weaponlib.WeaponSpawnEntity;
 import net.minecraft.util.DamageSource;
-import net.minecraft.util.EntityDamageSourceIndirect;
 
 public class ServerInterceptors {
+    private static final float KNOCKBACK_STRENGTH = 0.051f;
     
     public static float getKnockback(DamageSource source) {
         float strength = 0.4f;
-        if(source instanceof EntityDamageSourceIndirect) {
-            EntityDamageSourceIndirect indirectSource = (EntityDamageSourceIndirect) source;
-            if(indirectSource.getImmediateSource() instanceof WeaponSpawnEntity) {
-                strength = 0.05f;
-            }
+        if (source instanceof WeaponSpawnEntity.ProjectileDamageSource) {
+            strength = KNOCKBACK_STRENGTH;
         }
         return strength;
+    }
+
+    public static double getKnockback(float strength) {
+        return strength == KNOCKBACK_STRENGTH ? 1.0D : 2.0D;
     }
 }
