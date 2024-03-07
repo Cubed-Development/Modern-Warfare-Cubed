2729776293ea5f5996105183404bb7b985dfa866
Desoroxxx <desoroxxx@gmail.com>
2024-03-07 13:54:26 +0100

    New Buildscript

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+rootProject.name = "Modern-Warfare-Cubed"
+
+pluginManagement {
+    repositories {
+        maven {
+            // RetroFuturaGradle
+            name = "GTNH Maven"
+            url = uri("http://jenkins.usrv.eu:8081/nexus/content/groups/public/")
+            isAllowInsecureProtocol = true
+            mavenContent {
+                includeGroup("com.gtnewhorizons")
+                includeGroup("com.gtnewhorizons.retrofuturagradle")
+            }
+        }
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0" // Automatic toolchain provisioning
+}
