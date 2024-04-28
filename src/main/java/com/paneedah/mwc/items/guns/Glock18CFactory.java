8809102882eba00eca8207cb75bdc41ad95c89f0
Braiam23 <133471454+Braiam23@users.noreply.github.com>
2024-04-28 17:54:06 -0500

    Series Glock

--- src/main/java/com/paneedah/mwc/items/guns/Glock18CFactory.java
+++ src/main/java/com/paneedah/mwc/items/guns/Glock18CFactory.java
@@ -217,8 +217,8 @@ public class Glock18CFactory implements GunFactory {
                 
             .withFirstPersonPositioning(
 					new Transform()
-					.withPosition(-0.945000f, 4.065000f, -7.845000f)
-					.withRotation(0.000000f, 1.000000f, 10.681469f)
+					.withPosition(-0.945000f, 4.065000f, -6.945000f)
+					.withRotation(0.000f, 1.000f, 17.881469f)
 					.withRotationPoint(-0.12000000357627871, -0.36000001072883614, 0.040000001192092904)
                     .withScale(3.0, 3.0, 3.0)
                 )
