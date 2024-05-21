94679aa365ef09b0a6d21036ca1ffed10b9625e9
yor <yoonch1022@naver.com>
2024-05-22 03:03:30 +0900

    Fix Deprecated Call

--- src/main/java/com/paneedah/mwc/groovyscript/MWCGroovyPlugin.java
+++ src/main/java/com/paneedah/mwc/groovyscript/MWCGroovyPlugin.java
@@ -23,6 +23,6 @@ public class MWCGroovyPlugin implements GroovyPlugin {
 
     @Override
     public void onCompatLoaded(GroovyContainer<?> groovyContainer) {
-        groovyContainer.getVirtualizedRegistrar().addFieldsOf(this);
+        groovyContainer.getRegistrar().addFieldsOf(this);
     }
 }
