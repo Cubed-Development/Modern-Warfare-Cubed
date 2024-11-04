43e5c627f228da842a79f938b1c059fda733abf8
strubium <113206902+strubium@users.noreply.github.com>
2024-11-04 12:24:26 -0600

    ModernAnimator + LerpedValue

--- src/main/java/com/paneedah/weaponlib/numerical/LerpedValue.java
+++ src/main/java/com/paneedah/weaponlib/numerical/LerpedValue.java
@@ -1,19 +1,16 @@
 package com.paneedah.weaponlib.numerical;
 
 import com.paneedah.weaponlib.animation.MatrixHelper;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import static com.paneedah.mwc.proxies.ClientProxy.MC;
 
+@Getter @NoArgsConstructor
 public class LerpedValue {
 
     public double previousValue;
     public double currentValue;
-
-
-    public LerpedValue() {
-
-    }
-
 
     /**
      * Called before changes are made, sets the previous value to the
@@ -38,10 +35,6 @@ public class LerpedValue {
 
     public double getValue() {
         return this.currentValue;
-    }
-
-    public double getPreviousValue() {
-        return this.previousValue;
     }
 
     public double getLerped() {
