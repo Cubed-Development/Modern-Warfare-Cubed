057f0621488f0d7a244d6dc06c610a0ceb1228bc
strubium <113206902+strubium@users.noreply.github.com>
2024-11-04 08:03:02 -0600

    SpriteSheetTools + BalancePackManager

--- src/main/java/com/paneedah/weaponlib/config/BalancePackManager.java
+++ src/main/java/com/paneedah/weaponlib/config/BalancePackManager.java
@@ -2,6 +2,8 @@ package com.paneedah.weaponlib.config;
 
 import com.google.gson.*;
 import com.paneedah.weaponlib.Weapon;
+import lombok.Getter;
+import lombok.Setter;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
@@ -80,6 +82,7 @@ public class BalancePackManager {
         return PACK_MANAGER_VERSION;
     }
 
+    @Setter @Getter
     public static class GunCategoryBalanceConfiguration {
         private GunConfigurationGroup group;
         private double damageMultiplier = 1.0;
@@ -94,36 +97,8 @@ public class BalancePackManager {
             this.hipFireSpreadMultiplier = hipFireSpread;
         }
 
-        public GunConfigurationGroup getGroup() {
-            return group;
-        }
-
-        public void setGroup(GunConfigurationGroup group) {
-            this.group = group;
-        }
-
-        public double getDamageMultiplier() {
-            return damageMultiplier;
-        }
-
-        public void setDamageMultiplier(double damageMultiplier) {
-            this.damageMultiplier = damageMultiplier;
-        }
-
-        public double getRecoilMultiplier() {
-            return recoilMultiplier;
-        }
-
-        public void setRecoilMultiplier(double recoilMultiplier) {
-            this.recoilMultiplier = recoilMultiplier;
-        }
-
         public double getHipFireSpread() {
             return hipFireSpreadMultiplier;
-        }
-
-        public void setHipFireSpread(double hipFireSpread) {
-            this.hipFireSpreadMultiplier = hipFireSpread;
         }
 
         public JsonObject toJSONObject() {
@@ -148,6 +123,7 @@ public class BalancePackManager {
 
     }
 
+    @Getter @Setter
     public static class GunBalanceConfiguration {
         private String weaponName;
         private boolean enabled = true;
@@ -182,10 +158,6 @@ public class BalancePackManager {
             this.inaccuracy = inaccuracy;
         }
 
-        public String getWeaponName() {
-            return weaponName;
-        }
-
         public void setBurstShots(int shots) {
             if (shots != 0) {
                 fireModeBurstChanged = true;
@@ -218,10 +190,6 @@ public class BalancePackManager {
             return this.fireModeBurstChanged;
         }
 
-        public int getBurstShots() {
-            return burstShots;
-        }
-
         public boolean getFiremodeAuto() {
             return this.autoFireEnabled;
         }
@@ -234,44 +202,12 @@ public class BalancePackManager {
             return this.fireModePropertiesChanged;
         }
 
-        public void setWeaponName(String weaponName) {
-            this.weaponName = weaponName;
-        }
-
-        public boolean isEnabled() {
-            return enabled;
-        }
-
-        public void setEnabled(boolean enabled) {
-            this.enabled = enabled;
-        }
-
-        public double getDamage() {
-            return damage;
-        }
-
-        public void setDamage(double damage) {
-            this.damage = damage;
-        }
-
-        public double getRecoil() {
-            return recoil;
-        }
-
-        public void setRecoil(double recoil) {
-            this.recoil = recoil;
-        }
-
         public void setFirerate(float firerate) {
             this.fireRate = firerate;
         }
 
         public double getFirerate() {
             return this.fireRate;
-        }
-
-        public void setInaccuracy(float inaccuracy) {
-            this.inaccuracy = inaccuracy;
         }
 
         public double getInaccuracy() {
@@ -331,6 +267,7 @@ public class BalancePackManager {
         }
     }
 
+    @Setter @Getter
     public static class BalancePack {
         private String name;
         private String version;
@@ -455,63 +392,6 @@ public class BalancePackManager {
 
         }
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        public void setVersion(String version) {
-            this.version = version;
-        }
-
-        public double getGlobalRecoilMultiplier() {
-            return globalRecoilMultiplier;
-        }
-
-        public void setGlobalRecoilMultiplier(double globalRecoilMultiplier) {
-            this.globalRecoilMultiplier = globalRecoilMultiplier;
-        }
-
-        public double getGlobalDamageMultiplier() {
-            return globalDamageMultiplier;
-        }
-
-        public void setGlobalDamageMultiplier(double globalDamageMultiplier) {
-            this.globalDamageMultiplier = globalDamageMultiplier;
-        }
-
-        public double getGlobalHipFireSpread() {
-            return globalHipFireSpread;
-        }
-
-        public void setGlobalHipFireSpread(double globalHipFireSpread) {
-            this.globalHipFireSpread = globalHipFireSpread;
-        }
-
-        public HashMap<String, GunBalanceConfiguration> getGunConfigurations() {
-            return gunConfigurations;
-        }
-
-        public void setGunConfigurations(HashMap<String, GunBalanceConfiguration> gunConfigurations) {
-            this.gunConfigurations = gunConfigurations;
-        }
-
-        public HashMap<GunConfigurationGroup, GunCategoryBalanceConfiguration> getGunCategoryConfigurations() {
-            return gunCategoryConfigurations;
-        }
-
-        public void setGunCategoryConfigurations(
-                HashMap<GunConfigurationGroup, GunCategoryBalanceConfiguration> gunCategoryConfigurations) {
-            this.gunCategoryConfigurations = gunCategoryConfigurations;
-        }
-
         public boolean containsWeapon(String name) {
             return gunConfigurations.containsKey(name);
         }
@@ -563,7 +443,7 @@ public class BalancePackManager {
 
         } catch (IOException e) {
             LOGGER.catching(e);
-            LOGGER.error("Failed to create a new index.json");
+            LOGGER.error("Failed to remake index.json");
         }
     }
 
@@ -594,7 +474,7 @@ public class BalancePackManager {
             LOGGER.error("Failed to read file {} from the disk!", file.getName());
             return null;
         }
-        JsonObject object = null;
+        JsonObject object;
         try {
             object = GSON_MANAGER.fromJson(reader, JsonObject.class);
         } catch (JsonSyntaxException jse) {
@@ -700,7 +580,7 @@ public class BalancePackManager {
         String header = TextFormatting.GOLD + "(Balance Pack Manager " + BalancePackManager.getPackManagerVersion()
                 + ")" + TextFormatting.WHITE;
 
-        JsonObject object = null;
+        JsonObject object;
         try {
             object = GSON_MANAGER.fromJson(alledgedJson, JsonObject.class);
         } catch (JsonSyntaxException e) {
--- src/main/java/com/paneedah/weaponlib/render/SpriteSheetTools.java
+++ src/main/java/com/paneedah/weaponlib/render/SpriteSheetTools.java
@@ -1,7 +1,11 @@
 package com.paneedah.weaponlib.render;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class SpriteSheetTools {
 
+    @Setter @Getter
     public static class Sprite {
         private double minU, minV, maxU, maxV;
 
@@ -12,37 +16,6 @@ public class SpriteSheetTools {
             this.maxV = maxV;
         }
 
-        public double getMinU() {
-            return minU;
-        }
-
-        public void setMinU(double minU) {
-            this.minU = minU;
-        }
-
-        public double getMinV() {
-            return minV;
-        }
-
-        public void setMinV(double minV) {
-            this.minV = minV;
-        }
-
-        public double getMaxU() {
-            return maxU;
-        }
-
-        public void setMaxU(double maxU) {
-            this.maxU = maxU;
-        }
-
-        public double getMaxV() {
-            return maxV;
-        }
-
-        public void setMaxV(double maxV) {
-            this.maxV = maxV;
-        }
     }
 
     /**
