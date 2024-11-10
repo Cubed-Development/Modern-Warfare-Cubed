05148183eb6757ed9b681fe8bcdc85fc1634373a
Desoroxxx <desoroxxx@gmail.com>
2024-11-10 17:41:45 +0100

    Discard changes to src/main/java/com/paneedah/mwc/vehicle/McLarenSennaFactory.java

--- src/main/java/com/paneedah/mwc/vehicle/McLarenSennaFactory.java
+++ src/main/java/com/paneedah/mwc/vehicle/McLarenSennaFactory.java
@@ -1,5 +1,6 @@
 package com.paneedah.mwc.vehicle;
 
+import com.paneedah.mwc.MWC;
 import com.paneedah.mwc.models.*;
 import com.paneedah.mwc.vehicle.engines.McLarenSennaEngine;
 import com.paneedah.weaponlib.ModContext;
