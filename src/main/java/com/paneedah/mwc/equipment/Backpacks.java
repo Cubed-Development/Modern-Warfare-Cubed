aed03702f3f9f12052a1ff86df50cefdfcc0c9b8
strubium <113206902+strubium@users.noreply.github.com>
2024-12-09 18:14:55 -0600

    cleanup backpacks + fix lang

--- src/main/java/com/paneedah/mwc/equipment/Backpacks.java
+++ src/main/java/com/paneedah/mwc/equipment/Backpacks.java
@@ -7,25 +7,9 @@ import net.minecraft.item.Item;
 
 public class Backpacks {
 
-    public static Item combatSustainmentBackpackTan;
-    public static Item combatSustainmentBackpackBlack;
-    public static Item combatSustainmentBackpackForest;
-
-    public static Item assaultBackpackTan;
-    public static Item assaultBackpackBlack;
-    public static Item assaultBackpackForest;
-
-    public static Item truSpecCorduraBackpackTan;
-    public static Item truSpecCorduraBackpackBlack;
-    public static Item truSpecCorduraBackpackForest;
-
-    public static Item f5SwitchbladeBackpack;
-
-    public static Item duffleBag;
-
     public static void createEquipment(ModContext modContext) {
 
-        combatSustainmentBackpackTan = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("combat_sustainment_backpack_tan")
                 .withSize(10)
                 .withModel("CombatSustainmentBackpack")
@@ -42,7 +26,7 @@ public class Backpacks {
                         .applyTransformations())
                 .build();
 
-        combatSustainmentBackpackBlack = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("combat_sustainment_backpack_black")
                 .withSize(10)
                 .withModel("CombatSustainmentBackpack")
@@ -54,7 +38,7 @@ public class Backpacks {
                         .applyTransformations())
                 .build();
 
-        combatSustainmentBackpackForest = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("combat_sustainment_backpack_forest")
                 .withSize(10)
                 .withModel("CombatSustainmentBackpack")
@@ -66,56 +50,56 @@ public class Backpacks {
                         .applyTransformations())
                 .build();
 
-        assaultBackpackTan = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("assault_backpack_tan")
                 .withSize(16)
                 .withModel("AssaultBackpack")
                 .withTexture("equipment/carryable/backpacks/assault_backpack_tan")
                 .build();
 
-        assaultBackpackBlack = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("assault_backpack_black")
                 .withSize(16)
                 .withModel("AssaultBackpack")
                 .withTexture("equipment/carryable/backpacks/assault_backpack_black")
                 .build();
 
-        assaultBackpackForest = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("assault_backpack_forest")
                 .withSize(16)
                 .withModel("AssaultBackpack")
                 .withTexture("equipment/carryable/backpacks/assault_backpack_forest")
                 .build();
 
-        truSpecCorduraBackpackTan = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("tru_spec_cordura_backpack_tan")
                 .withSize(20)
                 .withModel("TruSpecCorduraBackpack")
                 .withTexture("equipment/carryable/backpacks/tru_spec_cordura_backpack_tan")
                 .build();
 
-        truSpecCorduraBackpackBlack = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("tru_spec_cordura_backpack_black")
                 .withSize(20)
                 .withModel("TruSpecCorduraBackpack")
                 .withTexture("equipment/carryable/backpacks/tru_spec_cordura_backpack_black")
                 .build();
 
-        truSpecCorduraBackpackForest = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("tru_spec_cordura_backpack_forest")
                 .withSize(20)
                 .withModel("TruSpecCorduraBackpack")
                 .withTexture("equipment/carryable/backpacks/tru_spec_cordura_backpack_forest")
                 .build();
 
-        f5SwitchbladeBackpack = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("f5_switchblade_backpack")
                 .withSize(16)
                 .withModel("F5SwitchbladeBackpack")
                 .withTexture("equipment/carryable/backpacks/f5_switchblade_backpack")
                 .build();
 
-        duffleBag = new ItemBackpack.Builder()
+        new ItemBackpack.Builder()
                 .withName("duffle_bag")
                 .withSize(24)
                 .withModel("Dufflebag")
