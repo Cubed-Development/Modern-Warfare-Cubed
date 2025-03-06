f267874736a145aa51b09f4a722d7e37a4e84875
strubium <113206902+strubium@users.noreply.github.com>
2025-03-06 10:26:22 -0600

    Move what can be done in init to init

--- src/main/java/com/paneedah/mwc/proxies/CommonProxy.java
+++ src/main/java/com/paneedah/mwc/proxies/CommonProxy.java
@@ -18,41 +18,14 @@ import com.paneedah.weaponlib.animation.SpecialAttachments;
 import com.paneedah.weaponlib.vehicle.network.VehicleDataSerializer;
 import com.paneedah.weaponlib.vehicle.network.VehiclePhysSerializer;
 import net.minecraft.network.datasync.DataSerializers;
+import net.minecraftforge.fml.common.ProgressManager;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 
-    /*
-    public static Item ElectronicCircuitBoard;
-    public static Item OpticGlass;
-    public static Item Cloth;
-    public static Item TanCloth;
-    public static Item GreenCloth;
-    public static Item Inductor;
-    public static Item Transistor;
-    public static Item Resistor;
-    public static Item Diode;
-    public static Item Capacitor;
-    public static Item CopperWiring;
-    public static Item Piston;
-    public static Item LaserPointer;
-    public static Item AluminumPlate;
-    public static Item SteelPlate;
-    public static Item BigSteelPlate;
-    public static Item MiniSteelPlate;
-    public static Item MetalComponents;
-    public static Item Plastic;
-    public static Item Backpack;
-    */
-
-
-    // I cannot figure out for the life of me why moving the init classes to the init() instead of the preInit() makes the game die, but I have no choice but to leave it here for now.
     public void preInit(final MWC mod) {
         MWC.modContext.preInit(mod);
-
-        UniversalSoundRegistry.init();
-        UniversalSoundLookup.initialize(MWC.modContext);
 
         // Forcing Item Initialization here, at the very least the variables, before they get registered normally on the init() (@SubscribeEvent) phase.
         MWCItems.init();
@@ -61,7 +34,7 @@ public class CommonProxy {
         DataSerializers.registerSerializer(VehicleDataSerializer.SERIALIZER);
         DataSerializers.registerSerializer(VehiclePhysSerializer.SERIALIZER);
 
-        // Special object initialization
+        // Special object initialization (Magic Mag)
         SpecialAttachments.init(MWC.modContext);
 
         Backpacks.createEquipment(MWC.modContext);
@@ -79,9 +52,6 @@ public class CommonProxy {
         Grenades.init(this);
         CustomSpawnEggs.init(this);
 
-        TurretBaseFactory.createTileEntity(MWC.modContext);
-        TileEntities.createTileEntity(MWC.modContext);
-
         new TacticalTomahawkFactory().createMelee(this);
         new BaseballBatFactory().createMelee(this);
         new BaseballBatNailsFactory().createMelee(this);
@@ -92,6 +62,12 @@ public class CommonProxy {
 
     public void init(final MWC mod) {
         MWC.modContext.init(mod);
+
+        UniversalSoundRegistry.init();
+        UniversalSoundLookup.initialize(MWC.modContext);
+
+        TurretBaseFactory.createTileEntity(MWC.modContext);
+        TileEntities.createTileEntity(MWC.modContext);
 
         Entities.init(MWC.modContext);
         Vehicles.init(MWC.modContext);
