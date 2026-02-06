ab072948b81fd7ac477d8531ac5d8e67c94637ce
voroxity <47036119+voroxity@users.noreply.github.com>
2026-02-06 16:43:59 -0500

    improved original fix

--- src/main/java/com/paneedah/weaponlib/crafting/ammopress/GUIContainerAmmoPress.java
+++ src/main/java/com/paneedah/weaponlib/crafting/ammopress/GUIContainerAmmoPress.java
@@ -102,11 +102,11 @@ public class GUIContainerAmmoPress extends GUIContainerStation<TileEntityAmmoPre
     @Override
     public void fillFilteredList() {
         filteredCraftingList.clear();
-        if (getCraftingMode() == 4) {
+        if (getCraftingMode() == CraftingGroup.BULLET.getID()) {
             filteredCraftingList.addAll(CraftingRegistry.getCraftingListForGroup(CraftingGroup.BULLET));
-        } else if (getCraftingMode() == 5) {
+        } else if (getCraftingMode() == CraftingGroup.MAGAZINE.getID()) {
             filteredCraftingList.addAll(CraftingRegistry.getCraftingListForGroup(CraftingGroup.MAGAZINE));
-        } else if (getCraftingMode() == 6) {
+        } else if (getCraftingMode() == CraftingGroup.GRENADE.getID()) {
             filteredCraftingList.addAll(CraftingRegistry.getCraftingListForGroup(CraftingGroup.GRENADE));
         }
     }
