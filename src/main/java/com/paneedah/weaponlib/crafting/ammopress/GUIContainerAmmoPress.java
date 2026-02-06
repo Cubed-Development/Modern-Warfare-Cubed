c3e244e0235a132984ebe82a2ef860f00c47087b
voroxity <47036119+voroxity@users.noreply.github.com>
2026-02-06 15:06:17 -0500

    revert setPage to inital value
    was not the fix

--- src/main/java/com/paneedah/weaponlib/crafting/ammopress/GUIContainerAmmoPress.java
+++ src/main/java/com/paneedah/weaponlib/crafting/ammopress/GUIContainerAmmoPress.java
@@ -85,7 +85,7 @@ public class GUIContainerAmmoPress extends GUIContainerStation<TileEntityAmmoPre
         addButton(magazineSelector);
         addButton(grenadeSelector);
 
-        setPage(2);
+        setPage(1);
     }
 
     /**
